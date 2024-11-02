3bc49319b897b15f048529056fcc2618c5206b52
Dinh Doan Xuan Phuong <115974903+phuong0111@users.noreply.github.com>
2024-11-02 10:44:00 +0700

    add test generator

---
+++ test-gen.py
@@ -0,0 +1,50 @@
+import os, sys, random
+
+PATH = "./tc/7/inp.txt"
+
+os.makedirs(os.path.dirname(PATH), exist_ok=True)
+
+PICKUP_CONTAINER = "PICKUP_CONTAINER"
+PICKUP_CONTAINER_TRAILER = "PICKUP_CONTAINER_TRAILER"
+DROP_CONTAINER = "DROP_CONTAINER"
+DROP_CONTAINER_TRAILER = "DROP_CONTAINER_TRAILER"
+PICKUP_TRAILER = "PICKUP_TRAILER"
+DROP_TRAILER = "DROP_TRAILER"
+STOP = "STOP"
+
+distance_range = (1, 10)
+operation_range = (1, 10)
+
+def get_rand(range: tuple[int, int]) -> int:
+    return random.randint(range[0], range[1])
+
+def gen(num_point: int, num_vehicles: int, num_requests: int):
+    print(f"Points {num_point}")
+    print(f"DISTANCES {num_point * num_point}")
+    distances = [[0 for j in range(0, num_point + 1)] for i in range(0, num_point + 1)]
+    
+    for i in range(1, num_point + 1):
+        for j in range(1, num_point + 1):
+            if i == j:
+                pass
+            elif i < j:
+                distances[i][j] = get_rand(distance_range)
+            else:
+                distances[i][j] = distances[j][i]
+            print(i, j, distances[i][j])
+    
+    print(f"TRAILER {get_rand((1, num_point))} {get_rand(operation_range)}")
+    print(f"TRUCK {num_vehicles}")
+    for _ in range(num_vehicles):
+        print(f"{_ + 1} {get_rand((1, num_point))}")
+    
+    for _ in range(num_requests):
+        print(f"REQ {_ + 1} {random.choice([20, 40])} {get_rand((1, num_point))} {random.choice([PICKUP_CONTAINER, PICKUP_CONTAINER_TRAILER])} {get_rand(operation_range)} {get_rand((1, num_point))} {random.choice([DROP_CONTAINER, DROP_CONTAINER_TRAILER])} {get_rand(operation_range)}")
+    print("#")
+    pass
+
+with open(PATH, "w") as fw:
+    sys.stdout = fw
+    gen(10, 10, 10)
+    
+    
